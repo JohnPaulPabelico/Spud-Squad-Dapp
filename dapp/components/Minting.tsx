b55a8d6bb6b1901472c54cbfaa08d050668858b4
Jp Pabelico <89491569+JohnPaulPabelico@users.noreply.github.com>
2024-04-20 18:31:23 +0800

    Small change

--- dapp/components/Minting.tsx
+++ dapp/components/Minting.tsx
@@ -99,7 +99,7 @@ export default function Minting() {
                   : "Loading..."}
               </div>
             ) : (
-              <div className="pixelify text-3xl lg:text-3xl text-bold py-2 px-4 text-center translate-y-12">
+              <div className="pixelify text-3xl lg:text-3xl text-bold py-2 px-4 text-center translate-y-12 mt-2">
                 Loading...
               </div>
             )}
